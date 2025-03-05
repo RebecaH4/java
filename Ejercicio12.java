dac76bdbad29fc3341a353452dfec42682aeb76b
RebecaH4 <161864810+RebecaH4@users.noreply.github.com>
2025-03-04 18:56:56 -0600

    Add files via upload

---
+++ Ejercicio12.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio12 {
+    public static void main(String[] args){
+        int num1;
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un numero de 1 o 2 digitos: "));
+
+        if(num1>0 && num1<=99){
+            if(num1<=9){
+                JOptionPane.showMessageDialog(null,"El numero tiene 1 digito");
+            }else{
+                JOptionPane.showMessageDialog(null,"El numero tiene 2 digitos");
+            }
+
+        }
+    }
+}
---
+++ Ejercicio13.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio13 {
+    public static void main(String[] args){
+        String password;
+
+        password = JOptionPane.showInputDialog("Ingresa la contraseña: ");
+
+        if(password.equals("tesji123")){
+            JOptionPane.showMessageDialog(null,"Correcto");
+        }else{
+            JOptionPane.showMessageDialog(null,"Error");
+        }
+
+    }
+}
