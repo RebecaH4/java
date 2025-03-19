d821f00057685be90febee5eab96b7ea65aff60a
RebecaH4 <161864810+RebecaH4@users.noreply.github.com>
2025-03-18 18:02:21 -0600

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,9 @@
+
+public class Ejercicio1{
+    public static void main(String[] args){
+        int a,b;
+        a = 12;
+        b = a * a;
+        System.out.println("La superficie delcuadrado es: " + b + "cm porque su lado es de " + a+"cm");
+    }
+}
