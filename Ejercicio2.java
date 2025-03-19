cddcf139c1011df899bdf7fbd001fe9e50b3ba6d
RebecaH4 <161864810+RebecaH4@users.noreply.github.com>
2025-03-19 14:59:18 -0600

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio2{
+    public static void main(String[] args){
+        int a, b, c, d;
+        a = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el primer numero: "));
+        b = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el segundo numero: "));
+
+        c = a + b;
+        d = a * b;
+
+        System.out.println("La suma de los numeros es: " + c + " El producto de los numeros es: "+ d);
+    }
+}
